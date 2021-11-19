77aaf562c1817da27dd2b5e257cdb521cb4adfd8
Matt F <trumpetman072392@gmail.com>
2021-11-19 16:40:35 -0600

    first commit

---
+++ Adventure-quest/Robe.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Quest
+{
+    public class Robe
+    {
+        public List<string> robeColor = new List<string>
+        {
+            "red",
+        };
+
+        public int robeLength = 10;
+    }
+}
